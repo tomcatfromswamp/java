6b4d61ab5de4d5b483ab45f45b24b6d55c299141
tomcatfromswamp <marshcat@gmail.com>
2016-02-18 18:11:36 +0300

    Support xml exchange from arduino

--- arduinoTHSmon/src/com/arduino/Main.java
+++ arduinoTHSmon/src/com/arduino/Main.java
@@ -4,6 +4,14 @@ package com.arduino;
         import jssc.SerialPortEvent;
         import jssc.SerialPortEventListener;
         import jssc.SerialPortException;
+        import org.w3c.dom.NodeList;
+        import org.xml.sax.InputSource;
+        import org.xml.sax.SAXException;
+
+        import javax.xml.crypto.dsig.XMLObject;
+        import javax.xml.parsers.DocumentBuilder;
+        import javax.xml.parsers.DocumentBuilderFactory;
+        import javax.xml.parsers.ParserConfigurationException;
         import java.io.*;
         import java.net.HttpURLConnection;
         import java.net.URL;
@@ -19,8 +27,10 @@ public class Main {
     private static String port;
     private static int interval;
     private static Boolean save;
-    private static String[] sensorsId;
     private static String cfgPath;
+    private static String compareData = "";
+    private static Boolean isBegin = false;
+    private static String servlet;
 
     public static void main(String[] args) throws IOException {
         if (args[0].equals("-c")) {
@@ -38,9 +48,9 @@ public class Main {
             debug = Boolean.parseBoolean(props.getProperty("debug"));
             name = props.getProperty("name");
             port = props.getProperty("port");
-            sensorsId = props.getProperty("sensors_id").split("\\,");
+            servlet = props.getProperty("servlet");
             interval = Integer.parseInt(props.getProperty("interval"));
-            System.out.println(name);
+            System.out.println("Имя: " + name);
             if (save) {
                 System.out.println("Работа в режиме сохранения данных в БД.");
             } else {
@@ -49,20 +59,16 @@ public class Main {
             serialPort = new SerialPort(port);
             try {
                 serialPort.openPort();
-                serialPort.setParams(SerialPort.BAUDRATE_9600,
-                        SerialPort.DATABITS_8,
-                        SerialPort.STOPBITS_1,
-                        SerialPort.PARITY_NONE);
-                serialPort.addEventListener(new PortReader(), SerialPort.MASK_RXCHAR);
+                serialPort.setParams(9600,8,1,0);
+                serialPort.setEventsMask(SerialPort.MASK_RXCHAR);
+                serialPort.addEventListener(new PortReader());
                 Timer timer = new Timer();
                 TimerTask timerTask = new TimerTask() {
                     @Override
                     public void run() {
                         try {
-                            for (String id : sensorsId) {
-                                serialPort.writeString(id);
+                                serialPort.writeString("0");
                                 Thread.sleep(1000);
-                            }
                         } catch (SerialPortException e) {
                             e.printStackTrace();
                         } catch (InterruptedException e) {
@@ -84,36 +90,66 @@ public class Main {
     public static class PortReader implements SerialPortEventListener {
 
         public void serialEvent(SerialPortEvent event) {
-            if(event.isRXCHAR()  && event.getEventValue() > 6){
+            if (event.isRXCHAR() && event.getEventValue() > 0) {
+                String compareDataXML = "";
                 try {
-                    String buffer = serialPort.readString();
-                    String[] sensors = buffer.split("\\,");
-                    int gpio;
-                    double temp,humi;
-                    if(sensors.length == 3) {
-                        gpio = Integer.parseInt(sensors[0]);
-                        temp = Double.parseDouble(sensors[1]);
-                        humi = Double.parseDouble(sensors[2]);
-                        System.out.println("Данные датчика на шине №" + gpio);
-                        System.out.println("Температура:  " + temp);
-                        System.out.println("Влажность:  " + humi);
-                        System.out.println("----------------------");
-                        if (save) {
-                            sendToServer(temp, humi, name, gpio);
+                    String buffer = serialPort.readString(event.getEventValue());
+                    //System.out.println("Буфер: " + buffer);
+                    if (buffer.contains("<?xml")) {
+                        //System.out.println("<--begin!");
+                        //System.out.println(buffer);
+                        compareData = buffer;
+                        isBegin = true;
+                    } else
+                    if (buffer.contains("</data>")) {
+                        //System.out.println("end-->");
+                        //System.out.println(buffer);
+                        compareDataXML = compareData + buffer;
+                        compareData = "";
+                        isBegin = false;
+                    } else {
+                        //System.out.println("--body--");
+                        //System.out.println(buffer);
+                        compareData = compareData + buffer;
+                        isBegin = false;
+                    }
+                    if (compareDataXML.length() > 0) {
+                        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                        DocumentBuilder db = dbf.newDocumentBuilder();
+                        org.w3c.dom.Document doc = db.parse(new InputSource(new StringReader(compareDataXML)));
+                        doc.getDocumentElement().normalize();
+                        NodeList nodes = doc.getElementsByTagName("sensor");
+                        System.out.println(nodes.getLength());
+                        for(int i=0;i<nodes.getLength();i++){
+                            int id = Integer.parseInt(nodes.item(i).getAttributes().getNamedItem("id").getNodeValue());
+                            double temp = Double.parseDouble(nodes.item(i).getAttributes().getNamedItem("temp").getNodeValue());
+                            String adr = nodes.item(i).getAttributes().getNamedItem("adr").getNodeValue();
+                            if (save) {
+                                sendToServer(temp, 0.0, name, id, adr);
+                            } else {
+                                System.out.println("Id : " + id);
+                                System.out.println("Addr : " + adr);
+                                System.out.println("Temp : " + temp);
+                                System.out.println("-------------------");
+                            }
                         }
                     }
-                }
-                catch (SerialPortException ex) {
-                    System.out.println(ex);
-                } catch (IOException e) {
+
+                }  catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ParserConfigurationException e) {
+                    e.printStackTrace();
+                } catch (SAXException e) {
+                    e.printStackTrace();
+                } catch (SerialPortException e) {
                     e.printStackTrace();
                 }
             }
         }
 
-        public void sendToServer(double temp, double humi, String name, int gpio) throws IOException {
+            public void sendToServer(double temp, double humi, String name, int id, String adr) throws IOException {
             final String USER_AGENT = "Mozilla/5.0";
-            String url = "http://weather-station.x5x.ru:8080/index.jsp?temp=" + temp + "&humi=" + humi + "&name=" + name + "&gpio=" + gpio;
+            String url = servlet + "?temp=" + temp + "&humi=" + humi + "&name=" + name + "&id=" + id + "&adr=" + adr;
             URL obj = new URL(url);
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
             con.setRequestMethod("GET");
